cb716afd08e6998de150b37b514e849bbc3caf96
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-09-07 13:39:54 -0300

    Add files via upload

---
+++ analyze/src/main/java/br/com/loanman/analyze/AnalyzeApplication.java
@@ -0,0 +1,13 @@
+package br.com.loanman.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzeApplication.class, args);
+	}
+
+}
---
+++ analyze/src/test/java/br/com/loanman/analyze/AnalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loanman.analyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
